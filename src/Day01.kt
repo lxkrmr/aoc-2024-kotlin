ad4badabccd277fb8900b2d8b3c9b5633d22ec04
Alexander Kramer <alexander.kramer@timetoact.de>
2025-09-15 16:38:17 +0200

    day01: part 1 - total distance

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,26 @@
+import kotlin.math.abs
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val lists = input
+            .map { it.split("\\s+".toRegex()) }
+            .map { it[0].toInt() to it[1].toInt() }
+            .unzip()
+        return lists.first.sorted()
+            .zip(lists.second.sorted())
+            .sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // Part 1: total distance
+    check(part1(readInput("Day01_test")) == 11)
+    part1(readInput("Day01")).println()
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    // Part 2: similarity score
+    // check(part2(readInput("Day01_test")) == 31)
+    // part2(readInput("Day01")).println()
 }
