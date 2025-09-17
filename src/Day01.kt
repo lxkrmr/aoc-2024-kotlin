f8191783d5fa5cd45b9bb07802584ee44b11626d
Alexander Kramer <alexander.kramer@timetoact.de>
2025-09-17 08:04:37 +0200

    refactor: Using substring before and after last

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,32 +1,42 @@
 import kotlin.math.abs
 
 fun main() {
+    try {
+        val testInput = readInput("Day01_test")
+        val puzzleInput = readInput("Day01")
 
-    fun part1(input: List<String>): Int {
-        val (left, right) = input.toLists()
-        return left.sorted()
-            .zip(right.sorted())
-            .sumOf { abs(it.first - it.second) }
+        // Part 1: total distance
+        part1(testInput).also { check(it == 11) { "Expected 11 but got $it" } }
+        part1(puzzleInput).println()
+
+        // Part 2: similarity score
+        part2(testInput).also { check(it == 31) { "Expected 31 but got $it" } }
+        part2(puzzleInput).println()
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 
-    fun part2(input: List<String>): Int {
-        val (left, right) = input.toLists()
-        return left.sumOf { locationId ->
-            val occurrences = right.count { locationId == it }
-            locationId * occurrences
-        }
+}
+
+fun part1(input: List<String>): Int {
+    val (left, right) = input.toLists()
+    return left.sorted()
+        .zip(right.sorted())
+        .sumOf { abs(it.first - it.second) }
+}
+
+fun part2(input: List<String>): Int {
+    val (left, right) = input.toLists()
+    return left.sumOf { locationId ->
+        val occurrences = right.count { locationId == it }
+        locationId * occurrences
     }
-
-    // Part 1: total distance
-    check(part1(readInput("Day01_test")) == 11)
-    part1(readInput("Day01")).println()
-
-    // Part 2: similarity score
-    check(part2(readInput("Day01_test")) == 31)
-    part2(readInput("Day01")).println()
 }
 
 fun List<String>.toLists(): Pair<List<Int>, List<Int>> = this
-    .map { it.split("\\s+".toRegex()) }
-    .map { it[0].toInt() to it[1].toInt() }
+    .map {
+        val first = it.substringBefore(' ').toInt()
+        val second = it.substringAfterLast(' ').toInt()
+        first to second
+    }
     .unzip()
