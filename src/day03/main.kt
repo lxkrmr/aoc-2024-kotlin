5fa5255788c0a3ca681c5b0142d15c5e22209b7a
Alexander Kramer <alexander.kramer@timetoact.de>
2025-09-23 13:42:35 +0200

    feat: first solution for part 1 and part 2

--- src/day03/main.kt
+++ src/day03/main.kt
@@ -13,8 +13,8 @@ fun main() {
         part1(puzzleInput).println()
 
         // Part 2: similarity score
-        // part2(testInput).also { check(it == 4) { "Expected 4 but got $it" } }
-        // part2(puzzleInput).println()
+        part2(testInput).also { check(it == 48) { "Expected 48 but got $it" } }
+        part2(puzzleInput).println()
     } catch (e: Exception) {
         e.printStackTrace()
     }
@@ -22,9 +22,34 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    return input.sumOf { line ->
+        "mul\\((\\d+),(\\d+)\\)".toRegex()
+            .findAll(line)
+            .sumOf { matchResult ->
+                matchResult.groupValues[1].toInt() * matchResult.groupValues[2].toInt()
+            }
+    }
 }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    var result = 0
+    var doMul = true
+
+    for (line in input) {
+        val findAll = """mul\((\d+),(\d+)\)|do\(\)|don't\(\)""".toRegex().findAll(line).toList()
+        for (matchResult in findAll) {
+            when(matchResult.value) {
+                "do()" -> doMul = true
+                "don't()" -> doMul = false
+                else -> {
+                    if (doMul) {
+                        result += matchResult.groupValues[1].toInt() * matchResult.groupValues[2].toInt()
+                    }
+                }
+            }
+        }
+    }
+
+
+    return result
 }
