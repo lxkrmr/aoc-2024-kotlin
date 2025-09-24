1600042cfda188fc97db1c7c314cbd1516c09e63
Alexander Kramer <alexander.kramer@timetoact.de>
2025-09-24 15:14:24 +0200

    feat: first wrong solution of part 1

--- src/day04/main.kt
+++ src/day04/main.kt
@@ -24,11 +24,61 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    input.println()
-    return 1
+    var result = 0
+    val grid = input.map { line -> line.toList() }
+    for (x in grid.indices) {
+        for (y in grid[x].indices) {
+            result += grid.horizontal(x, y).isXmasOrXmasReversed().toScore() +
+                    grid.vertical(x, y).isXmasOrXmasReversed().toScore() +
+                    grid.diagonalRight(x, y).isXmasOrXmasReversed().toScore() +
+                    grid.diagonalLeft(x, y).isXmasOrXmasReversed().toScore()
+        }
+    }
+    return result
 }
 
 fun part2(input: List<String>): Int {
     return 2
 }
 
+fun List<List<Char>>.horizontal(x: Int, y: Int): String {
+    return try {
+        val row = this[x]
+        row.subList(y, y + 4).joinToString("")
+    } catch (_: IndexOutOfBoundsException) {
+        ""
+    }
+}
+
+fun List<List<Char>>.vertical(x: Int, y: Int): String {
+    return try {
+        val column = this.map { row -> row[y] }
+        column.subList(x, x + 4).joinToString("")
+    } catch (_: IndexOutOfBoundsException) {
+        ""
+    }
+}
+
+fun List<List<Char>>.diagonalRight(x: Int, y: Int): String {
+    return try {
+        (0..<4).map {
+            this[x + it][y + it]
+        }.joinToString("")
+    } catch (_: IndexOutOfBoundsException) {
+        ""
+    }
+}
+
+fun List<List<Char>>.diagonalLeft(x: Int, y: Int): String {
+    return try {
+        (0..<4).map {
+            this[x - it][y - it]
+        }.joinToString("")
+    } catch (_: IndexOutOfBoundsException) {
+        ""
+    }
+}
+
+fun String.isXmasOrXmasReversed(): Boolean = this.lowercase() == "xmas" || this.lowercase() == "samx"
+
+fun Boolean.toScore(): Int = if (this) 1 else 0
